2362486293f1cb8e33994f2978d92b9baa441f86
AyaMYoussef <35687458+AyaMYousef@users.noreply.github.com>
2025-01-12 21:28:43 +0200

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import Footer from '../Footer/footer'
 import { Outlet } from 'react-router-dom'
+import ContactInfo from '../ContactInfo/ContactInfo'
 
 export default function Layout() {
     return (
         <>
             <Navbar />
             <Outlet></Outlet>
+            <ContactInfo/>
             <Footer />
         </>
     )
