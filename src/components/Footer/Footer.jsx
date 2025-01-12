be6fce5d58d21dbbbc3c8b6bfbde1098f58328f2
AyaMYoussef <35687458+AyaMYousef@users.noreply.github.com>
2025-01-12 21:28:57 +0200

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <>
-            <footer class="bg-dark text-white text-center py-3 mt-5 fixed-bottom">
+            <footer class="bg-dark text-white text-center py-3 mt-5">
                 <p>&copy; Copyright © Hamza Nouh 2022</p>
             </footer>
         </>
