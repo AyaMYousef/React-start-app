d762b509f1964d9a17f6d29a385ed96a10350e8e
AyaMYoussef <35687458+AyaMYousef@users.noreply.github.com>
2025-01-13 17:50:01 +0200

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <>
             <footer class="bg-dark text-white text-center py-3 mt-5">
-                <p>&copy; Copyright © Hamza Nouh 2022</p>
+                <p>&copy; Copyright © Aya 2025</p>
             </footer>
         </>
     )
